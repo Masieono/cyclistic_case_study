191616340c95c13e8bc9b20618ac05098b55126b
Masieono <112970744+Masieono@users.noreply.github.com>
2024-09-01 11:22:28 -0500

    finished cleaning

--- 03_data_cleaning.sql
+++ 03_data_cleaning.sql
@@ -1,9 +1,50 @@
+-- delete entries that contain null values
 
+DELETE FROM `cyclistic_case_study.raw_combined_data_added_columns`
 
--- filter out entries with null values 
+WHERE
+  start_station_name IS NULL OR
+  start_station_id IS NULL OR
+  end_station_name IS NULL OR
+  end_station_id IS NULL OR
+  end_lat IS NULL OR
+  end_lng IS NULL --removed 1,006,761 rows
 
--- filter out entries with negative time deltas
+-- delete entries with negative time deltas
+
+DELETE FROM `cyclistic_case_study.raw_combined_data_added_columns`
+
+WHERE
+  trip_duration < MAKE_INTERVAL(0) -- removed 116 rows
 
 -- filter out entries with trips over 24 hours long
 
+DELETE FROM `cyclistic_case_study.raw_combined_data_added_columns`
+
+WHERE
+  trip_duration > MAKE_INTERVAL(0, 0, 0, 24, 0, 0) -- removed 1,275 rows
+
 -- filter out entries with trips involving a testing station
+
+DELETE FROM `cyclistic_case_study.raw_combined_data_added_columns`
+
+WHERE
+  start_station_name LIKE '%Warehouse%' OR
+  start_station_name LIKE '%TEST%' OR
+  start_station_name LIKE '%DIVVY%' OR
+  start_station_name LIKE '%REPAIR%' OR
+
+  start_station_id LIKE '%Warehouse%' OR
+  start_station_id LIKE '%TEST%' OR
+  start_station_id LIKE '%DIVVY%' OR
+  start_station_id LIKE '%REPAIR%' OR
+
+  end_station_name LIKE '%Warehouse%' OR
+  end_station_name LIKE '%TEST%' OR
+  end_station_name LIKE 'DIVVY%' OR
+  end_station_name LIKE 'REPAIR%' OR
+
+  end_station_id LIKE '%Warehouse%' OR
+  end_station_id LIKE '%TEST%' OR
+  end_station_id LIKE '%DIVVY%' OR
+  end_station_id LIKE '%REPAIR%' -- removed 589 rows
